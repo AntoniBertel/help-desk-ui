c9c79082dd0428f2037d8e17f03b4181eea9ef64
Andrew Shapel <AndrewShapel@users.noreply.github.com>
2015-12-08 02:55:14 +0300

    Added cssnano (CSS optimizer) plugin to gulp

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,12 +2,14 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import importcss from 'gulp-import-css';
 import cssmin from 'gulp-cssmin';
+import cssnano from 'gulp-cssnano';
 import configuration from '../config';
 
 gulp.task('styles', () => {
   return gulp.src(configuration.tasks.styles.src)
     .pipe(importcss())
     .pipe(cssmin())
+    .pipe(cssnano())
     .pipe(gulp.dest(configuration.tasks.styles.dest));
 });
 
@@ -15,5 +17,6 @@ gulp.task('sass', () => {
   return gulp.src(configuration.tasks.sass.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(cssmin())
+    .pipe(cssnano())
     .pipe(gulp.dest(configuration.tasks.sass.dest));
 });
